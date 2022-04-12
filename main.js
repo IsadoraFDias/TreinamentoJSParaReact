83a2e2c4fe2e68366eb26cb882f5c7581098e6dd
Isadora Dias <diasfisadora@gmail.com>
2022-04-11 21:27:19 -0300

    atualizacao

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import './src/style/settings/colors.css'
 import './src/style/generic/generic.css'
 import './src/style/elements/base.css'
 import CardGame from "./src/components/cardGame";
-
 const $root = document.querySelector("#root");
 const $htmlCardGame = CardGame()
 
